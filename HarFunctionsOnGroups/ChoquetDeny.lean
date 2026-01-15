4c3fb9b1766dfc9eab63eefdb00cea0c8f6433da
Peter Kosenko <65818946+petekos-math@users.noreply.github.com>
2026-01-15 15:20:08 -0800

    Finished the proof of the lemma

--- HarFunctionsOnGroups/ChoquetDeny.lean
+++ HarFunctionsOnGroups/ChoquetDeny.lean
@@ -8,8 +8,8 @@ import Mathlib.MeasureTheory.Integral.Bochner.Basic
 
 noncomputable section
 
-lemma unif_bounded_diff (a : ℕ → ℝ) (b : ℝ) (abnd : ∃ C, ∀ n, |a n| ≤ C) (bpos : b ≥ 0) 
-  (hdiff : ∀ n, (a (n + 1) - a n) ≥ b) : b = 0 := by
+lemma unif_bounded_diff (a : ℕ → ℝ) (b : ℝ) (abnd : ∃ C, ∀ n, |a n| ≤ C)
+(bpos : b ≥ 0) (hdiff : ∀ n, (a (n + 1) - a n) ≥ b) : b = 0 := by
   by_contra bne0
   push_neg at bne0
   have bgt0 : b > 0 := Std.lt_of_le_of_ne bpos (id (Ne.symm bne0))
@@ -25,15 +25,32 @@ lemma unif_bounded_diff (a : ℕ → ℝ) (b : ℝ) (abnd : ∃ C, ∀ n, |a n| 
         _ = (n + 1) * b + a 0 :=  by ring
       simp
   rcases abnd with ⟨C, abndC⟩
-  have weird : ∀ (n : ℕ), n * b ≥ a 0 → |n * b + a 0| ≤ C := by
-    intro n h1
-    calc
-      |n * b + a 0| ≤ |a n| := by sorry
-      _ ≤ C := abndC n
-  have bigN : ∃ (N : ℕ),  N * b ≥ max |a 0| (C - a 0) := by sorry
-  rcases bigN with ⟨N, hN⟩
-  specialize weird N
-  sorry 
+  contrapose! abndC
+  let N := Nat.ceil ((|C| + |a 0|) / b) + 1
+  use N
+  specialize hbig N
+  calc
+    C ≤ |C| := le_abs_self C
+    _ ≤ |C| + (|a 0| + a 0) := by
+      simp only [le_add_iff_nonneg_right]
+      refine neg_le_iff_add_nonneg.mp ?_
+      exact neg_le_abs (a 0)
+    _ = (|C| + |a 0|) + a 0 := by ring
+    _ = ((|C| + |a 0|) / b) * b + a 0 := by
+      simp only [add_left_inj]
+      exact Eq.symm (div_mul_cancel₀ (|C| + |a 0|) bne0)
+    _ ≤ (N - 1) * b + a 0 := by
+      simp only [add_le_add_iff_right]
+      unfold N
+      simp only [Nat.cast_add, Nat.cast_one, add_sub_cancel_right]
+      rw [mul_le_mul_iff_left₀ bgt0]
+      exact Nat.le_ceil ((|C| + |a 0|) / b)
+    _ < N * b + a 0 := by
+      simp only [add_lt_add_iff_right]
+      rw [mul_lt_mul_iff_left₀ bgt0]
+      simp only [sub_lt_self_iff, zero_lt_one]
+    _ ≤ a N := RCLike.ofReal_le_ofReal.mp hbig
+    _ ≤ |a N| := by exact le_abs_self (a N)
 
 
 theorem ChoquetDeny {G : Type*} [AddCommGroup G] [TopologicalSpace G] [MeasurableSpace G]
@@ -67,14 +84,7 @@ theorem ChoquetDeny {G : Type*} [AddCommGroup G] [TopologicalSpace G] [Measurabl
   have hineq1 : ∀ (x : G), (Φ g) x ≥ g x := by sorry
   have gpos : ∀ (x : G), g x ≥ 0 := by sorry
   have h2 : ∀ (n : ℕ), ∀ (x : G), (Φ^[n+1] (fun (y : G) => |f y * f y|)) x - (Φ^[n] (|f * f|)) x ≥ g x := by sorry
-  apply unif_bounded_diff g x at h2
-
-
-  
-    
-    
-
-
+  sorry
 
 theorem ChoquetDenyPMF {G : Type*} [CommGroup G] [TopologicalSpace G] [DiscreteTopology G]
   [Countable G] (μ : PMF G) (f : G → ℝ)
